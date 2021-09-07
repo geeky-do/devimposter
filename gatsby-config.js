f9fb6be33926de764b1b44d04acebf4f758e7544
Dina Youssef <dina.youssef10@gmail.com>
2021-09-07 21:30:22 +0200

    Switch to posthog for analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,19 @@ module.exports = {
     title: "devimposter",
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-posthog-analytics`,
+      options: {
+        // Specify the API key for your Posthog Project (required)
+        apiKey: "phc_zWD1LIARPT1eaRy0M5cU4ssQ19ZYgvwhXkdUK6PdZSr",
+        // Specify the app host if self-hosting (optional, default: https://app.posthog.com)
+        appHost: "https://app.posthog.com",
+        // Puts tracking script in the head instead of the body (optional, default: true)
+        head: true,
+        // Enable posthog analytics tracking during development (optional, default: false)
+        isEnabledDevMode: true
+      },
+    },
     "gatsby-plugin-image",
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sitemap",
