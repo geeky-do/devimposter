29237e30544b21e192b9581b7487a7cb2ebedffd
Dina Youssef <dina.youssef10@gmail.com>
2021-09-08 11:57:07 +0200

    Add a basic blog

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,34 +1,23 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "devimposter",
+    siteUrl: "https://devimposter.tech",
+    title: "Dev Imposter",
+    description: "This is Robin"    
   },
   plugins: [
-    {
-      resolve: `gatsby-plugin-posthog-analytics`,
-      options: {
-        // Specify the API key for your Posthog Project (required)
-        apiKey: "phc_zWD1LIARPT1eaRy0M5cU4ssQ19ZYgvwhXkdUK6PdZSr",
-        // Specify the app host if self-hosting (optional, default: https://app.posthog.com)
-        appHost: "https://app.posthog.com",
-        // Puts tracking script in the head instead of the body (optional, default: true)
-        head: true,
-        // Enable posthog analytics tracking during development (optional, default: false)
-        isEnabledDevMode: true
-      },
-    },
-    "gatsby-plugin-image",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
-    {
-      resolve: "gatsby-plugin-manifest",
-      options: {
-        icon: "src/images/icon.png",
-      },
-    },
-    "gatsby-plugin-mdx",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
+    // {
+    //   resolve: `gatsby-plugin-posthog-analytics`,
+    //   options: {
+    //     // Specify the API key for your Posthog Project (required)
+    //     apiKey: "phc_zWD1LIARPT1eaRy0M5cU4ssQ19ZYgvwhXkdUK6PdZSr",
+    //     // Specify the app host if self-hosting (optional, default: https://app.posthog.com)
+    //     appHost: "https://app.posthog.com",
+    //     // Puts tracking script in the head instead of the body (optional, default: true)
+    //     head: true,
+    //     // Enable posthog analytics tracking during development (optional, default: false)
+    //     isEnabledDevMode: true
+    //   },
+    // },
     {
       resolve: "gatsby-source-filesystem",
       options: {
@@ -38,6 +27,13 @@ module.exports = {
       __key: "images",
     },
     {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `blogs`,
+        path: `${__dirname}/src/blogs/`,
+      },
+    },
+    {
       resolve: "gatsby-source-filesystem",
       options: {
         name: "pages",
@@ -45,5 +41,26 @@ module.exports = {
       },
       __key: "pages",
     },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "templates",
+        path: "./src/templates/",
+      },
+      __key: "templates",
+    },
+    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
+    },
+    "gatsby-plugin-image",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-mdx",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    `gatsby-transformer-remark`,
   ],
 };
---
+++ src/blogs/let-there-be-light.md
@@ -0,0 +1,10 @@
+---
+title: Awesome Blog Post Title
+author: Nikola
+slug: /blog/let-there-be-light
+date: 2020-07-15
+---
+
+## Introduction to my blog post
+
+Great content of my first blog
