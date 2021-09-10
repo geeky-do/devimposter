376c094fad86ef1a9554cc96921ca03d50445d92
Dina Youssef <dina.youssef10@gmail.com>
2021-09-10 16:56:16 +0200

    Add ruby script to use erb to create new posts

---
+++ new-post.rb
@@ -0,0 +1,32 @@
+require "erb"
+require 'fileutils'
+
+title = ARGV[0]
+
+# Create template.
+template = %q{
+---
+title: <%= title %>
+date: "Created"
+tags: []
+description: xxx 
+permalink: posts/{{ title | slug }}/index.html
+---
+  
+}.gsub( /^$\n/, '')
+
+message = ERB.new(template, 0, "%<>")
+
+
+# Produce result.
+email = message.result
+puts email
+
+
+dirname = "src/posts/"
+file_name = title.strip + ".md"
+FileUtils.mkdir_p(dirname) unless Dir.exists?(dirname)
+f = File.open(File.join(Dir.pwd, dirname, file_name),"w+")
+##fill the content
+f.write(email)
+f.close
